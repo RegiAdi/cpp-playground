8afa704ae756399e78f06d24e21620d7d7b5c73d
Regi Adi <regiadi.priyanto@gmail.com>
2024-06-20 00:49:32 +0700

    Add function definition reorder

--- function.cpp
+++ function.cpp
@@ -5,9 +5,16 @@
 
 int add(int x, int y); // forward declaration of add() (using a function declaration)
 
+// reorder the function definitions so subtract is defined before main
+int subtract(int x, int y)
+{
+	return x - y;
+}
+
 int main()
 {
 	std::cout << "2 + 1 = " << add(2, 1) << std::endl;
+	std::cout << "4 - 2 = " << subtract(4, 2) << std::endl;
 
 	return 0;
 }
