6836972cdb2a46226ae6a42f39fb8ca5fef1f00d
Regi Adi <regiadi.priyanto@gmail.com>
2024-07-15 10:41:12 +0700

    Add a math library example to cmake

--- cmake/cmake_tutorial.cpp
+++ cmake/cmake_tutorial.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "square.h"
 
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
+	std::cout << "Square Sides: " << getSquareSides() << std::endl;
+	std::cout << "Square Perimeter: " << getSquarePerimeter(5) << std::endl;
 
 	return 0;
 }
